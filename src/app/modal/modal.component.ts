d9ccc99d51bf9578231c703fed08e07e42568949
Daniel Geslin <dgeslin@belatrix.com>
2017-03-08 13:08:48 -0300

    modal

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent {
+  @Input() visible: boolean;
+  @Output() visibleChange: EventEmitter<boolean> = new EventEmitter();
 
   constructor() { }
 
-  ngOnInit() {
+  public open(){
+    this.visibleChange.emit(true);
+  }
+
+  public close(){
+    this.visibleChange.emit(false);
   }
 
 }
